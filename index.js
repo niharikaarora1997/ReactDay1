4e7de888ec2ecd6b72c001be2ee9793c85fb0c0a
Niharika <niharikaa@dewsolutions.in>
2022-12-24 20:57:04 +0530

    js dom manipulation

---
+++ index.js
@@ -0,0 +1,12 @@
+const list = document.createElement('ul');
+const list_item1 = document.createElement('li');
+list_item1.innerHTML = 'SPW:123';
+const list_item2 = document.createElement('li');
+list_item2.innerHTML = 'SPW:124';
+const list_item3 = document.createElement('li');
+list_item3.innerHTML = 'SPW:125';
+list.appendChild(list_item1);
+list.appendChild(list_item2);
+list.appendChild(list_item3);
+const root = document.getElementById('root');
+root.appendChild(list);
