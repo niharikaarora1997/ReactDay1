1363fa47c1622763d52f0fbc09d9f43a47e50222
Niharika <niharikaa@dewsolutions.in>
2022-12-24 21:13:25 +0530

    React CDN Dom manipulation

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const list = document.createElement('ul');
-const list_item1 = document.createElement('li');
-list_item1.innerHTML = 'SPW:123';
-const list_item2 = document.createElement('li');
-list_item2.innerHTML = 'SPW:124';
-const list_item3 = document.createElement('li');
-list_item3.innerHTML = 'SPW:125';
-list.appendChild(list_item1);
-list.appendChild(list_item2);
-list.appendChild(list_item3);
-const root = document.getElementById('root');
-root.appendChild(list);
+// const list = document.createElement('ul');
+// const list_item1 = document.createElement('li');
+// list_item1.innerHTML = 'SPW:123';
+// const list_item2 = document.createElement('li');
+// list_item2.innerHTML = 'SPW:124';
+// const list_item3 = document.createElement('li');
+// list_item3.innerHTML = 'SPW:125';
+// list.appendChild(list_item1);
+// list.appendChild(list_item2);
+// list.appendChild(list_item3);
+// const root = document.getElementById('root');
+// root.appendChild(list);
+
+const list_item1 = React.createElement('li', {}, 'SPW:123');
+const list_item2 = React.createElement('li', {}, 'SPW:123');
+const list_item3 = React.createElement('li', {}, 'SPW:123');
+const list = React.createElement('ul', { id: 'checklist' }, [
+  list_item1,
+  list_item2,
+  list_item3,
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(list);
